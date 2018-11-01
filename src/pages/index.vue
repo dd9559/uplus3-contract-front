f2b2630e4c921d07e322cba7c7d676320070eeda
mono <2016259976@qq.com>
2018-11-01 14:50:02 +0800

    兼容性

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,18 +9,14 @@
       <div class="slider">
         <el-menu
           :default-active="activeIndex"
+          unique-opened="true"
           class="el-menu-demo"
           @select="handleSelect"
           text-color="#333333"
           active-text-color="#478DE3">
-          <el-menu-item index="1">合同</el-menu-item>
-          <el-submenu index="setting">
-            <template slot="title">设置</template>
-            <el-menu-item index="contractTemplate">合同模板设置</el-menu-item>
-          </el-submenu>
-          <el-submenu index="postSigning">
-            <template slot="title">签后</template>
-            <el-menu-item index="postReceive">后期接收</el-menu-item>
+          <el-submenu :index="item.path" v-for="item in views" :key="item.id">
+            <template slot="title">{{item.name}}</template>
+            <el-menu-item :index="grade.path" v-for="grade in item.child" :key="grade.name">{{grade.name}}</el-menu-item>
           </el-submenu>
         </el-menu>
       </div>
@@ -38,13 +34,114 @@
         views:[
           {
             id:1,
-            name:'设置',
-            path:'setting'
+            name:'合同',
+            path:'1',
+            child:[
+              {
+                name:'交易合同',
+                path:''
+              }
+            ]
           },
           {
             id:2,
+            name:'财务',
+            path:'2',
+            child:[
+              {
+                name:'应收实收',
+                path:''
+              },
+              {
+                name:'收付款单',
+                path:''
+              },
+              {
+                name:'票据管理',
+                path:''
+              },
+              {
+                name:'房款监管',
+                path:''
+              },
+              {
+                name:'加盟账务',
+                path:''
+              }
+            ]
+          },
+          {
+            id:3,
+            name:'业绩',
+            path:'3',
+            child:[
+              {
+                name:'应收业绩',
+                path:''
+              },
+              {
+                name:'实收业绩',
+                path:''
+              },
+              {
+                name:'门店实收',
+                path:''
+              }
+            ]
+          },
+          {
+            id:4,
             name:'签后',
-            path:'postSigning'
+            path:'4',
+            child:[
+              {
+                name:'后期接收',
+                path:''
+              },
+              {
+                name:'后期管理',
+                path:''
+              },
+              {
+                name:'后期监控',
+                path:''
+              }
+            ]
+          },
+          {
+            id:5,
+            name:'设置',
+            path:'setting',
+            child:[
+              {
+                name:'合同模板设置',
+                path:''
+              },
+              {
+                name:'后期流程设置',
+                path:''
+              },
+              {
+                name:'公司账户设置',
+                path:''
+              },
+              {
+                name:'电子签章设置',
+                path:''
+              },
+              {
+                name:'款类设置',
+                path:''
+              },
+              {
+                name:'职务-权限设置',
+                path:''
+              },
+              {
+                name:'个人-权限设置',
+                path:''
+              }
+            ]
           }
         ]
       }
@@ -52,9 +149,9 @@
     methods: {
       handleSelect(key, keyPath) {
         // debugger
-        this.$router.push({
+        /*this.$router.push({
           path:`${keyPath.join('/')}`
-        })
+        })*/
       },
       linkTo:function (item) {
         this.$router.push({
