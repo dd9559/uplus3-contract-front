1e7affa86c6044e4d5ff61f8e74d30934571a394
mono <2016259976@qq.com>
2018-11-05 16:52:53 +0800

    添加收付款单及审核页 修改入口模板页

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 模块懒加载
 const Index = () => import('@/pages/index')
-const contractTemplate =()=>import('@/pages/setting/contractTemplate')
-const postProcess=()=>import('@/pages/setting/postProcess')
-const Company=()=>import('@/pages/setting/company')
-const postSigning =() =>import('@/pages/postSigning/index')
-const postManage =() =>import('@/pages/postSigning/postManage')
-const postMonitor =() =>import('@/pages/postSigning/postMonitor')
-const postReceive =() =>import('@/pages/postSigning/postReceive')
-const actualHarvest=()=>import('@/pages/finance/actualHarvest')
-const moneyCheck=()=>import('@/pages/finance/moneyCheck')
+const contractTemplate = () => import('@/pages/setting/contractTemplate')
+const postProcess = () => import('@/pages/setting/postProcess')
+const Company = () => import('@/pages/setting/company')
+const postSigning = () => import('@/pages/postSigning/index')
+const postManage = () => import('@/pages/postSigning/postManage')
+const postMonitor = () => import('@/pages/postSigning/postMonitor')
+const postReceive = () => import('@/pages/postSigning/postReceive')
+const actualHarvest = () => import('@/pages/finance/actualHarvest')
+const moneyCheck = () => import('@/pages/finance/moneyCheck')
+const Bill = () => import('@/pages/finance/bill')
+const billDetails = () => import('@/pages/finance/billDetails')
 
 Vue.use(Router)
 
@@ -19,26 +21,34 @@ export default new Router({
     {
       path: '/',
       component: Index,
-      children:[
+      children: [
         {
-          path:'contractTemplate',
-          component:contractTemplate
+          path: 'contractTemplate',
+          component: contractTemplate
         },
         {
-          path:'postProcess',
-          component:postProcess
+          path: 'postProcess',
+          component: postProcess
         },
         {
-          path:'company',
-          component:Company
+          path: 'company',
+          component: Company
         },
         {
-          path:'actualHarvest',
-          component:actualHarvest
+          path: 'actualHarvest',
+          component: actualHarvest
         },
         {
-          path:'moneyCheck',
-          component:moneyCheck
+          path: 'moneyCheck',
+          component: moneyCheck
+        },
+        {
+          path: 'Bill',
+          component: Bill
+        },
+        {
+          path: 'billDetails',
+          component: billDetails
         }
       ]
     }
