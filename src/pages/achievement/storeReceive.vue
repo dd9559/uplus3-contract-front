639e9fdba9a49f2a23c91b2f03e5e5ab46399fbf
zhousong <15827447511@126.com>
2018-11-05 15:54:00 +0800

    配置业绩路由

---
+++ src/pages/achievement/storeReceive.vue
@@ -0,0 +1,19 @@
+<template>   
+     <div class="layout">
+        门店实收
+    </div>
+</template>
+
+<script>
+export default {
+    name:"storeReceive"
+}
+</script>
+
+<style scoped lang="less">
+  .layout{
+      padding: 30px;
+  }
+   
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,45 +2,59 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 模块懒加载
 const Index = () => import('@/pages/index')
-const contractTemplate =()=>import('@/pages/setting/contractTemplate')
-const postProcess=()=>import('@/pages/setting/postProcess')
-const Company=()=>import('@/pages/setting/company')
-const postSigning =() =>import('@/pages/postSigning/index')
-const postManage =() =>import('@/pages/postSigning/postManage')
-const postMonitor =() =>import('@/pages/postSigning/postMonitor')
-const postReceive =() =>import('@/pages/postSigning/postReceive')
-const actualHarvest=()=>import('@/pages/finance/actualHarvest')
-const moneyCheck=()=>import('@/pages/finance/moneyCheck')
+const contractTemplate = () => import('@/pages/setting/contractTemplate')
+const postProcess = () => import('@/pages/setting/postProcess')
+const Company = () => import('@/pages/setting/company')
+const postSigning = () => import('@/pages/postSigning/index')
+const postManage = () => import('@/pages/postSigning/postManage')
+const postMonitor = () => import('@/pages/postSigning/postMonitor')
+const postReceive = () => import('@/pages/postSigning/postReceive')
+const actualHarvest = () => import('@/pages/finance/actualHarvest')
+const moneyCheck = () => import('@/pages/finance/moneyCheck')
+
+//业绩模块
+const actualAchievement = () => import('@/pages/achievement/actualAchievement')
+const receivableAchievement = () => import('@/pages/achievement/receivableAchievement')
+const storeReceive = () => import('./../pages/achievement/storeReceive.vue')
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Index,
-      children:[
-        {
-          path:'contractTemplate',
-          component:contractTemplate
-        },
-        {
-          path:'postProcess',
-          component:postProcess
-        },
-        {
-          path:'company',
-          component:Company
-        },
-        {
-          path:'actualHarvest',
-          component:actualHarvest
-        },
-        {
-          path:'moneyCheck',
-          component:moneyCheck
-        }
-      ]
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: Index,
+    children: [{
+        path: 'contractTemplate',
+        component: contractTemplate
+      },
+      {
+        path: 'postProcess',
+        component: postProcess
+      },
+      {
+        path: 'company',
+        component: Company
+      },
+      {
+        path: 'actualHarvest',
+        component: actualHarvest
+      },
+      {
+        path: 'moneyCheck',
+        component: moneyCheck
+      },
+      {
+        path: 'actualAchievement',
+        component: actualAchievement
+      },
+      {
+        path: 'receivableAchievement',
+        component: receivableAchievement
+      },
+      {
+        path: 'storeReceive',
+        component: storeReceive
+      }
+    ]
+  }]
 })
