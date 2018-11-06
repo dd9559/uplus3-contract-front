cb8001acad7ec4837afebec1819042c9dfaa61f0
hujingcao <253128931@qq.com>
2018-11-06 14:28:26 +0800

    修改路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 模块懒加载
-const Index = () => import('@/pages/index')
-const contractTemplate =()=>import('@/pages/setting/contractTemplate')
-const postProcess=()=>import('@/pages/setting/postProcess')
-const Company=()=>import('@/pages/setting/company')
-const postSigning =() =>import('@/pages/postSigning/index')
-const postManage =() =>import('@/pages/postSigning/postManage')
-const postMonitor =() =>import('@/pages/postSigning/postMonitor')
-const postReceive =() =>import('@/pages/postSigning/postReceive')
-const actualHarvest=()=>import('@/pages/finance/actualHarvest')
-const moneyCheck=()=>import('@/pages/finance/moneyCheck')
-const newIntention = () => import("@/pages/contract/contractList/newIntention");
-Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: "/",
-      component: Index,
-      children: [
-        {
-          path: "contractTemplate",
-          component: contractTemplate
-        },
-        {
-          path: "postProcess",
-          component: postProcess
-        },
-        {
-          path: "company",
-          component: Company
-        },
-        {
-          path: "actualHarvest",
-          component: actualHarvest
-        },
-        {
-          path: "moneyCheck",
-          component: moneyCheck
-        },
-        // 合同——合同列表——新增意向金
-        {
-          path: "newIntention",
-          component: newIntention
-        }
-      ]
-    }
-  ]
-});
 const contractTemplate = () => import('@/pages/setting/contractTemplate')
 const postProcess = () => import('@/pages/setting/postProcess')
 const Company = () => import('@/pages/setting/company')
@@ -69,7 +22,8 @@ const moneyCheck = () => import('@/pages/finance/moneyCheck')
 const Bill = () => import('@/pages/finance/bill')
 const billDetails = () => import('@/pages/finance/billDetails')
 
-
+// 合同模块
+const newIntention = () => import("@/pages/contract/contractList/newIntention");
 Vue.use(Router)
 
 export default new Router({
@@ -119,6 +73,11 @@ export default new Router({
       {
         path: 'billDetails',
         component: billDetails
+      },
+      // 合同——合同列表——新增意向金
+      {
+        path: "newIntention",
+        component: newIntention
       }]
   }]
 })
