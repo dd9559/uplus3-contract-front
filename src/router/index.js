3639ca0fa0d61375eae0b4188e1492c81148ff92
hujingcao <253128931@qq.com>
2018-11-08 14:41:29 +0800

    1、新增“编辑意向金”，“意向金详情” 2、修改“新增意向金” 3、增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -25,61 +25,75 @@ const billDetails = () => import('@/pages/finance/billDetails')
 
 // 合同模块
 const newIntention = () => import("@/pages/contract/contractList/newIntention");
+const editIntention = () => import("@/pages/contract/contractList/editIntention");
+const detailIntention = () => import("@/pages/contract/contractList/detailIntention");
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    component: Index,
-    children: [{
-      path: 'contractTemplate',
-      component: contractTemplate
-    },
-      {
-        path: 'postProcess',
-        component: postProcess
-      },
-      {
-        path: 'company',
-        component: Company
-      },
-      {
-        path: 'actualHarvest',
-        component: actualHarvest
-      },
-      {
-        path: 'moneyCheck',
-        component: moneyCheck
-      },
-      {
-        path: 'actualAchievement',
-        component: actualAchievement
-      },
-      {
-        path: 'receivableAchievement',
-        component: receivableAchievement
-      },
-      {
-        path: 'storeReceive',
-        component: storeReceive
-      },
-      {
-        path: 'paperSet',
-        component: paperSet
-      },
-      {
-        path: 'Bill',
-        component: Bill
-      },
-      {
-        path: 'billDetails',
-        component: billDetails
-      },
-      // 合同——合同列表——新增意向金
-      {
-        path: "newIntention",
-        component: newIntention
-      }]
-  }]
-})
+  routes: [
+    {
+      path: "/",
+      component: Index,
+      children: [
+        {
+          path: "contractTemplate",
+          component: contractTemplate
+        },
+        {
+          path: "postProcess",
+          component: postProcess
+        },
+        {
+          path: "company",
+          component: Company
+        },
+        {
+          path: "actualHarvest",
+          component: actualHarvest
+        },
+        {
+          path: "moneyCheck",
+          component: moneyCheck
+        },
+        {
+          path: "actualAchievement",
+          component: actualAchievement
+        },
+        {
+          path: "receivableAchievement",
+          component: receivableAchievement
+        },
+        {
+          path: "storeReceive",
+          component: storeReceive
+        },
+        {
+          path: "paperSet",
+          component: paperSet
+        },
+        {
+          path: "Bill",
+          component: Bill
+        },
+        {
+          path: "billDetails",
+          component: billDetails
+        },
+        // 合同——合同列表——新增意向金
+        {
+          path: "newIntention",
+          component: newIntention
+        },
+        {
+          path: "editIntention",
+          component: editIntention
+        },
+        {
+          path: "detailIntention",
+          component: detailIntention
+        }
+      ]
+    }
+  ]
+});
 
