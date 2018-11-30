262a5df4dbccb03c7c74253fdf8d19fb4872d7fb
chenwei <chen_wei_rr@163.com>
2018-11-30 09:08:11 +0800

    合同无效接口调试 合同资料库接口调试

--- src/pages/contract/contractList/contractList.vue
+++ src/pages/contract/contractList/contractList.vue
@@ -223,10 +223,10 @@
           <template slot-scope="scope">
             <div style="text-align:center">
               <el-button type="text" size="medium" v-if="scope.row.contState.value!=3" @click="upload(scope.row.id)">上传</el-button>
-              <el-button type="text" size="medium" @click="goPreview">预览</el-button>
-              <!-- <el-button type="text" size="medium" v-if="scope.row.toExamineState.value===6" @click="goCheck(scope.row)">审核</el-button>  -->
+              <el-button type="text" size="medium" @click="goPreview(scope.row)">预览</el-button>
+              <el-button type="text" size="medium" v-if="scope.row.toExamineState.value===6" @click="goCheck(scope.row)">审核</el-button> 
               <el-button type="text" size="medium" @click="toLayerAudit(scope.row)">调佣</el-button>
-              <!-- <el-button type="text" size="medium" v-if="scope.row.toExamineState.value<0||scope.row.toExamineState.value===8" @click="submitAudit(scope.row)">提审</el-button> -->
+              <el-button type="text" size="medium" v-if="scope.row.toExamineState.value<0||scope.row.toExamineState.value===8" @click="submitAudit(scope.row)">提审</el-button>
             </div>
           </template>
         </el-table-column>
@@ -427,11 +427,11 @@ export default {
       }
     },
     //合同预览
-    goPreview() {
+    goPreview(item) {
       this.$router.push({
         path: "/contractPreview",
         query: {
-          id: 1
+          id: item.id
         }
       });
     },
