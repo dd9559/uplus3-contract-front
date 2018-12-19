882810c6d193c1c9a3bb3e3f6217024a30afe004
mono <2016259976@qq.com>
2018-12-19 15:58:54 +0800

    xu

--- src/assets/js/mixins.js
+++ src/assets/js/mixins.js
@@ -27,6 +27,7 @@ const MIXINS = {
   },
   created(){
     for (let item in this.power){
+      // debugger
       if(this.getUser){
         if(this.getUser.privileges.indexOf(item)>-1){
           this.power[item].state=true
