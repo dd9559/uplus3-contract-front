e2cdb996ebccfd0615aaaf843950f54dfcb01922
chenwei <chen_wei_rr@163.com>
2019-01-04 10:21:53 +0800

    完善

--- src/pages/contract/contractList/addContract.vue
+++ src/pages/contract/contractList/addContract.vue
@@ -1032,7 +1032,7 @@ export default {
             this.dialogSave=false;
             this.detailCode=res.data.code;
             this.detailId=res.data.id;
-            if(res.data.isHaveNextAudit===2){
+            if(res.data.isHaveNextAudit===1){
               this.checkPerson.flowType=3;
               this.checkPerson.code=res.data.code;
               this.checkPerson.state=true;
