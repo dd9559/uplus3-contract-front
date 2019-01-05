84350423699ff7dcfcafeccb32553b9121837d00
mono <2016259976@qq.com>
2019-01-05 14:10:45 +0800

    Bug:26634、26668、26770

--- src/main.js
+++ src/main.js
@@ -85,6 +85,7 @@ router.beforeEach((to,from,next)=>{
   store.commit('setPath',pathList?JSON.parse(pathList):[])
 
   // let userMsg = localStorage.getItem('userMsg')
+  console.log(store.state.user)
   if(!store.state.user){
     // debugger
     api.get('/api/me').then(res=>{
