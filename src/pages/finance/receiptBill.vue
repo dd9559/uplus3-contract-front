1a5d62fe97e3b26c07360577e0e906b3f7e2569c
mono <2016259976@qq.com>
2019-01-10 15:05:24 +0800

    xu

--- src/pages/finance/receiptBill.vue
+++ src/pages/finance/receiptBill.vue
@@ -14,7 +14,7 @@
                 :value="item.value">
               </el-option>
             </el-select>
-            <input type="text" size="small" class="w140 el-input__inner person" placeholder="请输入" v-model.trim="form.outObj" v-if="inputPerson">
+            <input type="text" size="small" class="w140 el-input__inner person" placeholder="请输入" maxlength="20" v-model.trim="form.outObj" v-if="inputPerson">
           </div>
         </div>
         <div class="input-group col active-400">
