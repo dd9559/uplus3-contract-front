c59857640c2cbdc39fc0cac2c05cfcb01403669d
chenwei <chen_wei_rr@163.com>
2019-03-05 15:09:08 +0800

    完善新增合同证件类型验证

--- src/pages/contract/contractList/addContract.vue
+++ src/pages/contract/contractList/addContract.vue
@@ -757,7 +757,7 @@ export default {
                             if ((element.propertyRightRatio&&element.propertyRightRatio>0)||element.propertyRightRatio==='0'||this.contractForm.type===1) {
                               if (element.encryptionCode) {
                                 let reg = /^[1-9]\d{5}((((19|[2-9][0-9])\d{2})(0?[13578]|1[02])(0?[1-9]|[12][0-9]|3[01]))|(((19|[2-9][0-9])\d{2})(0?[13456789]|1[012])(0?[1-9]|[12][0-9]|30))|(((19|[2-9][0-9])\d{2})0?2(0?[1-9]|1[0-9]|2[0-8]))|(((1[6-9]|[2-9][0-9])(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))0?229))\d{3}[0-9Xx]$/;
-                                if (element.cardType===1&&reg.test(element.encryptionCode)||element.cardType!==1) {
+                                if ((this.contractForm.type===1&&(element.cardType===1&&reg.test(element.encryptionCode)||element.cardType!==1))||(this.contractForm.type!==1&&reg.test(element.encryptionCode))) {
                                   isOk = true;
                                   ownerRightRatio += element.propertyRightRatio - 0;
                                 }else{
@@ -859,7 +859,7 @@ export default {
                                 if ((element.propertyRightRatio&&element.propertyRightRatio>0)||element.propertyRightRatio==='0'||this.contractForm.type===1) {
                                   if (element.encryptionCode) {
                                     let reg = /^[1-9]\d{5}((((19|[2-9][0-9])\d{2})(0?[13578]|1[02])(0?[1-9]|[12][0-9]|3[01]))|(((19|[2-9][0-9])\d{2})(0?[13456789]|1[012])(0?[1-9]|[12][0-9]|30))|(((19|[2-9][0-9])\d{2})0?2(0?[1-9]|1[0-9]|2[0-8]))|(((1[6-9]|[2-9][0-9])(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))0?229))\d{3}[0-9Xx]$/;
-                                    if (element.cardType===1&&reg.test(element.encryptionCode)||element.cardType!==1) {
+                                    if ((this.contractForm.type===1&&(element.cardType===1&&reg.test(element.encryptionCode)||element.cardType!==1))||(this.contractForm.type!==1&&reg.test(element.encryptionCode))) {
                                       isOk_ = true;
                                       guestRightRatio += element.propertyRightRatio - 0;
                                     }else{
