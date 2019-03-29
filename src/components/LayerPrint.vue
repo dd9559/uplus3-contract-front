e304c4fd50ab5c32e245bb7befc445c539ba6266
疯小孩 <343406717@qq.com>
2019-03-29 14:01:05 +0800

    打印修改

--- src/components/LayerPrint.vue
+++ src/components/LayerPrint.vue
@@ -1,140 +1,180 @@
 <template>
-    <div>
-        <div id="printHtml">
-            <slot></slot>
-        </div>
-        <iframe src="" id="printf" frameborder="0"></iframe>
+  <div>
+    <div id="printHtml">
+      <slot></slot>
     </div>
+    <!-- <iframe src="" id="printf" frameborder="0"></iframe> -->
+  </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    print(printCallBack) {
-      var printhtml = document.getElementById("printHtml").innerHTML;
-      var f = document.getElementById("printf");
-      // printhtml = `
-      // <div style='position:relative;z-index:100;'>${printhtml}</div>
-      // <div style='position:absolute;z-index:99;left:0;top:0;right:0;bottom:0;background-color:#fff'></div>
-      // `
-      if (navigator.userAgent.indexOf("Firefox") > 0) {
-        printhtml = `
+  export default {
+    methods: {
+      print(printCallBack) {
+        var printhtml = document.getElementById("printHtml").innerHTML;
+        var f = document.getElementById("printf");
+        // printhtml = `
+        // <div style='position:relative;z-index:100;'>${printhtml}</div>
+        // <div style='position:absolute;z-index:99;left:0;top:0;right:0;bottom:0;background-color:#fff'></div>
+        // `
+        if (navigator.userAgent.indexOf("Firefox") > 0) {
+          printhtml = `
                             <div>${printhtml}</div>
                             <div></div>
                             `;
-      } else {
-        printhtml = `
+        } else {
+          printhtml = `
                             <div style='position:relative;z-index:100;'>${printhtml}</div>
                             <div style='position:absolute;z-index:99;left:0;top:0;right:0;bottom:0;background-color:#fff'></div>
                             `;
-      }
-      // html
-      f.contentDocument.body.innerHTML = printhtml;
-      // css
-      this.getStyle(f, printCallBack);
-    },
-    // PageSetup_Null() {
-    //         var hkey_root, hkey_path, hkey_key;
-    //         hkey_path = "HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\PageSetup\\";
-    //         try {
-    //             var RegWsh = new ActiveXObject("WScript.Shell");
-    //             RegWsh.RegWrite(hkey_path + "header", "");
-    //             RegWsh.RegWrite(hkey_path + "footer", "");
-    //         } catch (e) {}
-    // },
-    getStyle(printI, printCallBack) {
-      var str = "",
-        styles1 = document.querySelectorAll("style");
-      for (var i = 0; i < styles1.length; i++) {
-        str += styles1[i].outerHTML;
-      }
-      str += `<style>header, footer {display: none;}</style>`;
-      printI.contentDocument.head.innerHTML = str;
+        }
+        // html
+        f.contentDocument.body.innerHTML = printhtml;
+        // css
+        // this.getStyle(f, printCallBack);
+        // 关闭
+        setTimeout(()=>{
+            try {
+                window.frames['printf'].print();
+            } catch (e) {
+                f.contentWindow.print();
+            }
+            printCallBack();
+        },100)
+      },
+      getStyle(printI) {
+        var str = "",
+          styles1 = document.querySelectorAll("style");
+        for (var i = 0; i < styles1.length; i++) {
+          str += styles1[i].outerHTML;
+        }
+        str += `<style>header, footer {display: none;}</style>`;
+        //   printI.contentDocument.head.innerHTML = str;
+        printI.contentDocument.head.innerHTML = str;
 
-      if (navigator.userAgent.indexOf("Firefox") > 0) {
-        lastBack();
-        return false;
-      }
-      // 添加link引入
-      let styles = document.querySelectorAll("link");
-      let lastLink = "";
-      for (let i = 0; i < styles.length; i++) {
+        //   console.log(printI.contentDocument.head)
+
+        //   if (navigator.userAgent.indexOf("Firefox") > 0) {
+        //     lastBack();
+        //     return false;
+        //   }
+        // 添加link引入
+        let styles = document.querySelectorAll("link");
+        let lastLink = "";
+        for (let i = 0; i < styles.length; i++) {
           // chrome 正常，firefox不正常，能执行到，但是添加没结果
           let link = document.createElement("link");
           link.setAttribute("rel", "stylesheet");
-          if (styles[i].type){
-              link.setAttribute("type", styles[i].type)
-          }else{
-              link.setAttribute("type", 'text/css')
+          if (styles[i].type) {
+            link.setAttribute("type", styles[i].type)
+          } else {
+            link.setAttribute("type", 'text/css')
           };
           // link.setAttribute("href", `${styles[i].href}?time=${new Date().getTime()}`);
           // link.setAttribute("href", styles[i].href);
           link.setAttribute("href", styles[i].href);
           link.setAttribute('media', 'all');
           printI.contentDocument.head.appendChild(link);
-          if(styles.length-1 === i){
-              lastLink = link;
+          if (styles.length - 1 === i) {
+            lastLink = link;
           }
-      }
-      this.loadCss(lastLink,lastBack)
-      function lastBack(){
-          // window.frames['printf'].focus();
-          // printI.contentDocument.close();
-          // var explorer = window.navigator.userAgent ;
-          try {
-              window.frames['printf'].print();
-          } catch (e) {
-              printI.contentWindow.print();
+        }
+        // this.loadCss(lastLink)
+      },
+      loadCss(node, callback) {
+        // for IE6-9 and Opera
+        if (node.attachEvent) {
+          node.attachEvent("onload", callback);
+          // NOTICE:
+          // 1. "onload" will be fired in IE6-9 when the file is 404, but in
+          // this situation, Opera does nothing, so fallback to timeout.
+          // 2. "onerror" doesn't fire in any browsers!
+        }
+        // polling for Firefox, Chrome, Safari
+        else {
+          setTimeout(function () {
+            poll(node, callback);
+          }, 0); // for cache
+        }
+
+        function poll(node, callback) {
+          if (callback.isCalled) {
+            return;
           }
-          printCallBack();
+          var isLoaded = false;
+          if (node["sheet"]) {
+            isLoaded = true;
+          }
+
+          if (isLoaded) {
+            // give time to render.
+            setTimeout(callback, 1);
+          } else {
+            setTimeout(function () {
+              poll(node, callback);
+            }, 1);
+          }
+        }
+      },
+      lastBack(printCallBack) {
+        // window.frames['printf'].focus();
+        // printI.contentDocument.close();
+        // var explorer = window.navigator.userAgent ;
+        try {
+          window.frames['printf'].print();
+        } catch (e) {
+          printI.contentWindow.print();
+        }
+        printCallBack();
+      },
+      init() {
+        var printI = document.getElementById("printf");
+        if (!printI) {
+            printI = document.createElement("iframe");
+            printI.id = "printf";
+            printI.style.position = 'fixed'
+            printI.style.width = '0'
+            printI.style.height = '0'
+            printI.style.top = '-100px'
+
+            // 兼容ie
+            if (
+                window.location.hostname !== document.domain &&
+                navigator.userAgent.match(/msie/i)
+            ) {
+                
+                printI.src =
+                    'javascript:document.write("<head><script>document.domain=\\"' +
+                    document.domain +
+                    '\\";</s' +
+                    'cript></head><body></body>")';
+                
+            }
+            printI.onload = () => {
+                this.getStyle(printI);
+            }
+                
+            document.body.appendChild(printI);
+        }else{
+            this.getStyle(printI);
+        } 
       }
     },
-    loadCss(node, callback) {
-      // for IE6-9 and Opera
-      if (node.attachEvent) {
-        node.attachEvent("onload", callback);
-        // NOTICE:
-        // 1. "onload" will be fired in IE6-9 when the file is 404, but in
-        // this situation, Opera does nothing, so fallback to timeout.
-        // 2. "onerror" doesn't fire in any browsers!
-      }
-      // polling for Firefox, Chrome, Safari
-      else {
-        setTimeout(function() {
-          poll(node, callback);
-        }, 0); // for cache
-      }
+    mounted() {
+      this.init();
+    },
+  };
 
-      function poll(node, callback) {
-        if (callback.isCalled) {
-          return;
-        }
-        var isLoaded = false;
-        if (node["sheet"]) {
-          isLoaded = true;
-        }
-
-        if (isLoaded) {
-          // give time to render.
-          setTimeout(callback, 1);
-        } else {
-          setTimeout(function() {
-            poll(node, callback);
-          }, 1);
-        }
-      }
-    }
-  }
-};
 </script>
 
 <style>
-#printf {
-  width: 100px;
-  height: 100px;
-  position: fixed;
-  top: -100px;
-  left: -100px;
-  z-index: 0;
-}
+  #printf {
+    width: 100px;
+    height: 100px;
+    position: fixed;
+    top: -100px;
+    left: -100px;
+    z-index: 0;
+  }
+
 </style>
