149c0643b5cdbdf08b41c4e343d3d78965528c7c
chenwei <chen_wei_rr@163.com>
2019-04-03 16:26:36 +0800

    修复h5页返回编辑页面 数据错误问题

--- src/pages/contract/contractList/addContract.vue
+++ src/pages/contract/contractList/addContract.vue
@@ -503,7 +503,7 @@ export default {
   created() {
     let backMsg = JSON.parse(localStorage.getItem("backMsg"));
     if(backMsg){//存在则是从h5页面返回  需走编辑逻辑
-      let contMsg = JSON.parse(localStorage.getItem("contractMsg"));
+      let contMsg = JSON.parse(sessionStorage.getItem("contractMsg"));
       this.contractForm.type = parseInt(contMsg.type);//合同类型
       this.type=2;//编辑
       this.id=parseInt(contMsg.id)
