1a8b72c131ff39ac6e6ec8defe969612a869cf10
chenwei <chen_wei_rr@163.com>
2019-07-17 11:59:43 +0800

    修复bug：35073 35071

--- src/pages/setting/lowCommission.vue
+++ src/pages/setting/lowCommission.vue
@@ -34,7 +34,7 @@
           <span class="title"><i class="iconfont icon-tubiao-11"></i>数据列表</span>
         </div>
         <div>
-          <el-button class="btn-info" round type="primary" size="small" @click="dialogRatio=true">新增低佣比例</el-button>
+          <el-button class="btn-info" icon="el-icon-plus" round type="primary" size="small" @click="dialogRatio=true">新增低佣比例</el-button>
         </div>
       </div>
       <el-table ref="tableCom" class="info-scrollbar" :data="tableData" style="width: 100%" border :max-height="tableNumberCom">
@@ -55,12 +55,14 @@
         </el-table-column>
         <el-table-column align="center" label="设置时间" min-width="50">
           <template slot-scope="scope">
-            <p v-for="item in scope.row.list" :key="item.id">{{item.settingTime|formatTime}}</p>
+            <!-- <p v-for="item in scope.row.list" :key="item.id">{{item.settingTime|formatTime}}</p> -->
+            {{scope.row.list[0].settingTime|formatTime}}
           </template>
         </el-table-column>
         <el-table-column align="center" label="最新修改时间" min-width="50">
           <template slot-scope="scope">
-            <p v-for="item in scope.row.list" :key="item.id">{{item.updateTime|formatTime}}</p>
+            <!-- <p v-for="item in scope.row.list" :key="item.id">{{item.updateTime|formatTime}}</p> -->
+            {{scope.row.list[0].updateTime|formatTime}}
           </template>
         </el-table-column>
         <el-table-column align="center" label="操作" min-width="120">
@@ -118,6 +120,36 @@
         <el-button round type="primary" @click="save">保存</el-button>
       </span>
     </el-dialog>
+    <!-- 保存比例确认框 -->
+    <el-dialog title="确认保存" :visible.sync="dialogSave" class="personalMsg" width="500px" :closeOnClickModal="$tool.closeOnClickModal">
+      <div class="warning-box">
+        <p>确认保存新的低佣比例设置？</p>
+        <p>新的低佣比例值会覆盖原来的低佣比例值</p>
+        <p>当前设置低佣比例区间为：</p>
+        <ul>
+          <li v-for="(item,index) in ratioList" :key="index">
+            <span>成交总价：{{item.dealPriceMin+'万 - '}}{{item.dealPriceMax?item.dealPriceMax:'999999999.99'}}万</span>
+            <span>低佣比例：<span v-for="item_ in dictionary['652']" :key="item_.key" v-if="item.judgeType===item_.key">{{item_.value}} </span>{{item.comPercentMin+' %'}}</span>
+          </li>
+        </ul>
+        <!-- <el-table :data="ratioList" border header-row-class-name="theader-bg">
+          <el-table-column label="成交总价">
+            <template slot-scope="scope">
+              <span>{{scope.row.dealPriceMin+'万 - '}}{{scope.row.dealPriceMax?scope.row.dealPriceMax:'999999999.99'}}万</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="比例">
+            <template slot-scope="scope">
+              <span v-for="item_ in dictionary['652']" :key="item_.key" v-if="scope.row.judgeType===item_.key">{{item_.value}} </span>{{scope.row.comPercentMin+' %'}}
+            </template>
+          </el-table-column>
+        </el-table> -->
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogSave = false" size="small">取消</el-button>
+        <el-button type="primary" size="small" @click="addEdit">确认</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
            
@@ -151,6 +183,7 @@ export default{
       total:0,
       dialogTitle:'新增低佣比例',
       dialogRatio:false,
+      dialogSave:false,
       forbid:false,//弹窗体系是否禁用
       systemType:'',//弹窗体系
       dictionary: {
@@ -254,21 +287,19 @@ export default{
       if(this.systemType){
         let isOK = false
         // this.ratioList.every((element,index) => {
-        for(var i=0;i<this.ratioList.length;i++){
+          let ratioList_=JSON.parse(JSON.stringify(this.ratioList))
+        for(var i=0;i<ratioList_.length;i++){
           isOK=false
-          if((this.ratioList[i].dealPriceMin.toString()).length>0&&Number(this.ratioList[i].dealPriceMax)>0){
-            if((this.ratioList[i].comPercentMin.toString()).length>0){
-              if(Number(this.ratioList[i].dealPriceMax)>Number(this.ratioList[i].dealPriceMin)){
+          if(!ratioList_[i].dealPriceMin){
+            ratioList_[i].dealPriceMin="0"
+          }
+          if(!ratioList_[i].dealPriceMax){
+            ratioList_[i].dealPriceMax="999999999.99"
+          }
+          if((ratioList_[i].dealPriceMin.toString()).length>0&&Number(ratioList_[i].dealPriceMax)>0){
+            if((ratioList_[i].comPercentMin.toString()).length>0){
+              if(Number(ratioList_[i].dealPriceMax)>Number(ratioList_[i].dealPriceMin)){
                 isOK=true
-                // if(Number(this.ratioList[i].comPercentMax)>Number(this.ratioList[i].comPercentMin)){
-                //   isOK=true
-                // }else{
-                //   this.$message({
-                //     message:"低佣比例范围错误",
-                //     type:"warning"
-                //   })
-                //   return
-                // }
               }else{
                 this.$message({
                   message:"成交总价范围错误",
@@ -299,14 +330,14 @@ export default{
           }else{//false 新增
             url="/api/commissionsetting/operateInsert"
           }
-          let listLength=this.ratioList.length
+          let listLength=ratioList_.length
           if(listLength>1){
             let beginList = []
             let endList = []
-            this.ratioList.forEach(element => {
+            ratioList_.forEach(element => {
               beginList.push(element.dealPriceMin)
             });
-            this.ratioList.forEach(element => {
+            ratioList_.forEach(element => {
               endList.push(element.dealPriceMax)
             });
             beginList.sort(this.compare)
@@ -323,49 +354,51 @@ export default{
               }
             }
             if(interval){
+              this.dialogSave=true
               // alert("完全OJBK！！！")
-              let param = {
-                cityId:'',
-                systemTag:this.systemType,
-                list:[].concat(this.ratioList)
-              }
-              this.$ajax.postJSON(url,param).then(res=>{
-                res=res.data
-                if(res.status===200){
-                  this.$message({
-                    message:"添加成功",
-                    type:"success"
-                  })
-                  this.closeDialog()
-                }
-              }).catch(error=>{
-                this.$message({
-                  message:error,
-                  type:"error"
-                })
-              })
+              // let param = {
+              //   cityId:'',
+              //   systemTag:this.systemType,
+              //   list:[].concat(this.ratioList)
+              // }
+              // this.$ajax.postJSON(url,param).then(res=>{
+              //   res=res.data
+              //   if(res.status===200){
+              //     this.$message({
+              //       message:"添加成功",
+              //       type:"success"
+              //     })
+              //     this.closeDialog()
+              //   }
+              // }).catch(error=>{
+              //   this.$message({
+              //     message:error,
+              //     type:"error"
+              //   })
+              // })
             }
           }else{
-            let param = {
-              cityId:'',
-              systemTag:this.systemType,
-              list:[].concat(this.ratioList)
-            }
-            this.$ajax.postJSON(url,param).then(res=>{
-              res=res.data
-              if(res.status===200){
-                this.$message({
-                  message:"添加成功",
-                  type:"success"
-                })
-                this.closeDialog()
-              }
-            }).catch(error=>{
-              this.$message({
-                message:error,
-                type:"error"
-              })
-            })
+            this.dialogSave=true
+            // let param = {
+            //   cityId:'',
+            //   systemTag:this.systemType,
+            //   list:[].concat(this.ratioList)
+            // }
+            // this.$ajax.postJSON(url,param).then(res=>{
+            //   res=res.data
+            //   if(res.status===200){
+            //     this.$message({
+            //       message:"添加成功",
+            //       type:"success"
+            //     })
+            //     this.closeDialog()
+            //   }
+            // }).catch(error=>{
+            //   this.$message({
+            //     message:error,
+            //     type:"error"
+            //   })
+            // })
           }
         }
       }else{
@@ -374,6 +407,38 @@ export default{
           type:"warning"
         })
       }
+    },
+    //新增编辑
+    addEdit(){
+      let url=''
+      let title="添加成功"
+      if(this.forbid){//true 编辑
+        url="/api/commissionsetting/operateUpdate"
+        title="编辑成功"
+      }else{//false 新增
+        url="/api/commissionsetting/operateInsert"
+      }
+      let param = {
+        cityId:'',
+        systemTag:this.systemType,
+        list:[].concat(this.ratioList)
+      }
+      this.$ajax.postJSON(url,param).then(res=>{
+        res=res.data
+        if(res.status===200){
+          this.$message({
+            message:title,
+            type:"success"
+          })
+          this.dialogSave=false
+          this.closeDialog()
+        }
+      }).catch(error=>{
+        this.$message({
+          message:error,
+          type:"error"
+        })
+      })
     },
     compare(a,b){
       return a-b
@@ -481,6 +546,29 @@ export default{
     }
   }
 } 
+.warning-box{
+  >p{
+    font-size: 14px;
+    text-align: center;
+    &:first-of-type{
+      margin: 5px 0;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+  ul{
+    margin-top: 10px;
+    li{
+      padding-left: 40px;
+      width: 450px;
+      >span{
+        padding: 2px 0;
+        display: inline-block;
+        width: 48%;
+      }
+    }
+  }
+}
 .top{
   padding-left: 20px;
   .system{
