fc42000ec2e98a67e5cf592bd4031fc5360f118c
zhoufei <418823891@qq.com>
2019-07-22 11:12:27 +0800

    1.手续费设置确认弹窗调整 2.负责的页面蓝色按钮色号统一

--- src/pages/setting/approvalProcess.vue
+++ src/pages/setting/approvalProcess.vue
@@ -34,7 +34,7 @@
         <div class="aduit-list">
             <p v-if="power['sign-set-verify'].state">
                 <span><i class="iconfont icon-tubiao-11 mr-8"></i>数据列表</span>
-                <el-button @click="operation('添加',1)">添加</el-button>
+                <el-button @click="operation('添加',1)" type="primary">添加</el-button>
             </p>
             <div class="table">
                 <el-table :data="tableData" style="width: 100%" border ref="tableCom" :max-height="tableNumberCom">
@@ -181,7 +181,7 @@
                 </div>
             </div>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="isSave" v-dbClick>保存</el-button>
+                <el-button @click="isSave" v-dbClick type="primary">保 存</el-button>
             </div>
         </el-dialog>
     </div>
@@ -901,7 +901,6 @@
       height:32px;
       padding: 4px 0;
       border-radius:18px;
-      background-color: #478DE3;
       color: #fff;
     }
   }
@@ -1093,7 +1092,6 @@
         width: 100px;
         height: 38px;
         border-radius: 19px;
-        background-color: #478DE3;
         color: #fff;
     }
 }
