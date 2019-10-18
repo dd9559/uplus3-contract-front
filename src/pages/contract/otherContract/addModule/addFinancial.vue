dda9d1705d3f0ddfaedccdae769b65e03efa7862
chenwei <chen_wei_rr@163.com>
2019-10-18 11:52:12 +0800

    金融成本比例限制为100以内

--- src/pages/contract/otherContract/addModule/addFinancial.vue
+++ src/pages/contract/otherContract/addModule/addFinancial.vue
@@ -42,7 +42,7 @@
             <i class="yuan">元</i>
           </el-form-item>
           <el-form-item label="金融成本比例：" class="form-label" style="width:280px;text-align:right;">
-            <input type="text" v-model="contractForm.financeCostRatio" @input="cutNumber('dealPrice')" @change="countCost" placeholder="请输入" class="dealPrice">
+            <input type="text" v-model="contractForm.financeCostRatio" @input="cutNumber('financeCostRatio')" @change="countCost" placeholder="请输入" class="dealPrice">
             <i class="yuan">%</i>
           </el-form-item>
           <el-form-item label="金融成本：" class="form-label width-250">
@@ -459,9 +459,14 @@ export default {
         })
       }else if(val==="financeCost"){
         this.$nextTick(()=>{
-          this.contractForm.financeCost=this.$tool.cutFloat({val:this.contractForm.financeCost,max:100})
+          this.contractForm.financeCost=this.$tool.cutFloat({val:this.contractForm.financeCost,max:999999999.99})
+        })
+      }else if(val==="financeCostRatio"){
+        this.$nextTick(()=>{
+          this.contractForm.financeCostRatio=this.$tool.cutFloat({val:this.contractForm.financeCostRatio,max:100})
         })
       }
+      // financeCostRatio
     },
     //姓名限制
     inputOnly(type){
