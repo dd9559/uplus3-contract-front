ff16042c235a3deaf13fc4addb26218df4b2271d
liufei <837326723@qq.com>
2019-11-12 16:05:56 +0800

    解决bug  业绩详情没有上传协议时显示协议文字

--- src/pages/achievement/achDetial.vue
+++ src/pages/achievement/achDetial.vue
@@ -631,7 +631,7 @@ export default{
             this.houseArr = data.data.houseAgents;
             this.clientArr = data.data.customerAgents;
             this.serviceach=data.data.serviceAgents
-            if (data.data.distributionAgreement&&JSON.parse(data.data.distributionAgreement).length>0) {
+            if (res.data.data.distributionAgreement&&JSON.parse(res.data.data.distributionAgreement).length>0) {
             this.fujian=true
             this.tradeFee=res.data.data.tradeFee
             // this.tradeFee=
--- src/pages/achievement/achPage.vue
+++ src/pages/achievement/achPage.vue
@@ -2788,7 +2788,7 @@ export default {
             this.$set(this.serviceAgents[i],'checkbox',[])
             this.serviceAgents[i].checkbox.push(this.serviceAgents[i].place)
           }
-          if (data.data.distributionAgreement&&JSON.parse(data.data.distributionAgreement).length>0) {
+          if (res.data.data.distributionAgreement&&JSON.parse(res.data.data.distributionAgreement).length>0) {
             let pathList = JSON.parse(res.data.data.distributionAgreement);
             this.filesList = pathList;
             let preloadList = [];
