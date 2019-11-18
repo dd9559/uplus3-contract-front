1f658e3862242b88a51a981cba8c0ef9d2784369
mono <2016259976@qq.com>
2019-11-18 11:43:45 +0800

    芜湖租赁委托合同文件名称重命名及js地址修改

--- src/pages/iframe/iframeTest.vue
+++ src/pages/iframe/iframeTest.vue
@@ -19,7 +19,7 @@ import {MIXINS_DJ} from "../contract/mixins/DJ.js";
     mixins: [MIXINS_MM],
     data(){
       return{
-        src:'WTDBXY.html'
+        src:'WHU-委托.html'
       }
     },
     created(){
