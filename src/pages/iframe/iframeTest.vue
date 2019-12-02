d1541e538b87d9ba2c00d6659e1c10c3195654ac
zhoufei <418823891@qq.com>
2019-12-02 11:48:42 +0800

    XN买卖js

--- src/pages/iframe/iframeTest.vue
+++ src/pages/iframe/iframeTest.vue
@@ -19,7 +19,7 @@ import {MIXINS_DJ} from "../contract/mixins/DJ.js";
     mixins: [MIXINS_MM],
     data(){
       return{
-        src:'wh新委托.html'
+        src:'XN-买卖.html'
       }
     },
     created(){
