4727300d7d2fb3bdcc60855f4801faa6eaf69502
zhoufei <418823891@qq.com>
2020-04-20 09:37:36 +0800

    zhou

--- src/assets/js/mixins.js
+++ src/assets/js/mixins.js
@@ -29,7 +29,8 @@ const MIXINS = {
       tableBoxCom:null,
       tableNumberCom:null,
       systemTagList: [],
-      systemTagSelect: []
+      systemTagSelect: [],
+      accountDepList: [], //记账本新增均摊支出部门
     }
   },
   watch:{
@@ -127,6 +128,7 @@ const MIXINS = {
         res=res.data
         if(res.status===200){
           this.DepList=[].concat(res.data)
+          this.accountDepList = res.data.map((item=>Object.assign({},item)))
           this.Loading=false
         }
       })
