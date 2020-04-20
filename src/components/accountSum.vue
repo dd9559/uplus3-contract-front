f69dbeff2bd70afe3bcbb1a9504039e22a61b738
zhoufei <418823891@qq.com>
2020-04-20 09:36:07 +0800

    记账本账目汇总和支出弹窗需求

---
+++ src/components/accountSum.vue
@@ -0,0 +1,206 @@
+<template>
+    <div class="view-container" ref="tableComView">
+        <ScreeningTop
+        @propQueryFn="queryFn"
+        @propResetFormFn="resetFormFn">
+            <el-form :inline="true" size="small">
+                <el-form-item label="月份">
+                    <el-date-picker
+                    v-model="sumDate"
+                    type="monthrange"
+                    range-separator="至"
+                    start-placeholder="开始月份"
+                    value-format="yyyy-MM"
+                    end-placeholder="结束月份">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="部门">
+                    <select-tree :data="DepList" :init="depName" @checkCell="depHandleClick" @clear="clearDep"
+                         @search="searchDep"></select-tree>
+                </el-form-item>
+            </el-form>
+        </ScreeningTop>
+        <div class="view-context">
+            <div class="table-tool">
+                <ul>
+                    <li>
+                        收入<span class="color-green">{{tableHead.income}}</span>元
+                    </li>
+                    <li>
+                        支出<span class="color-red">{{tableHead.expenditure}}</span>元
+                    </li>
+                    <li>
+                        利润<span class="color-red">{{tableHead.profit}}</span>元
+                    </li>
+                </ul>
+                <p>
+                    <el-button class="btn-info" size="small" @click="getExcel">导出1</el-button>
+                    <el-button class="btn-info" size="small" @click="addFn()">导出1</el-button>
+                    <el-button class="btn-info" size="small" @click="addFn(2)">导出2</el-button>
+                </p>
+            </div>
+            <el-table :data="list" border header-row-class-name="theader-bg" style="width: 100%">
+                <el-table-column label="月份" prop="moneyTime"></el-table-column>
+                <el-table-column label="部门" prop="deptName"></el-table-column>
+                <el-table-column label="收入（元）" prop="income">
+                    <template slot-scope="scope">
+                        <span :class="{'color-green':scope.row.income>0}">{{scope.row.income}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="支出（元）" prop="expenditure">
+                    <template slot-scope="scope">
+                        <span :class="{'color-red':scope.row.expenditure>0}">{{scope.row.expenditure}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="利润（元）" prop="profit"></el-table-column>
+            </el-table>
+            <el-pagination
+            v-if="total"
+            class="pagination-info"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="pageNum"
+            :page-size="pageSize"
+            layout="total, prev, pager, next, jumper"
+            :total="total">
+            </el-pagination>
+        </div>
+
+        <addPay :dialogVisible="openPay" :version="version" @closePayDialog="closePay" @refreshData="getData" v-if="openPay"></addPay>
+    </div>
+</template>
+
+<script>
+import {MIXINS} from "@/assets/js/mixins";
+import addPay from "@/components/addPay";
+
+export default {
+    name: "account-sum",
+    mixins: [MIXINS],
+    components: {
+        addPay,
+    },
+    data() {
+        return {
+            sumDate: [],
+            deptId: '',
+            depName: '',
+            list: [],
+            total: 0,
+            pageNum: 1,
+            pageSize: 20,
+            openPay: false,
+            version: '',
+            tableHead: {
+                expenditure: 0,
+                income: 0,
+                profit: 0
+            }
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        getData() {
+            let param = {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize,
+                deptId: this.deptId
+            }
+            if(this.sumDate && this.sumDate.length) {
+                param.moneyTimeStart = this.sumDate[0]
+                param.moneyTimeEnd = this.sumDate[1]
+            }
+            this.$ajax.get('/api/accountBook/accountTotal', param).then(res => {
+                res = res.data
+                if(res.status === 200) {
+                    this.list = res.data.list.list
+                    this.total = res.data.list.total
+                    this.tableHead = {...res.data}
+                }
+            })
+        },
+        getExcel() {
+            let param = {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize,
+                deptId: this.deptId
+            }
+            if(this.sumDate && this.sumDate.length) {
+                param.moneyTimeStart = this.sumDate[0]
+                param.moneyTimeEnd = this.sumDate[1]
+            }
+            this.excelCreate('/input/accountBookExcel',param)
+        },
+        addFn(type=1) {
+            this.openPay = true
+            this.version = type
+        },
+        closePay() {
+            this.openPay = false
+        },
+        clearDep: function () {
+            this.depName = ''
+            this.clearSelect()
+        },
+        searchDep: function (payload) {
+            // this.clearSelect('emp')
+        },
+        depHandleClick(data) {
+            this.deptId = data.depId
+            this.depName = data.name
+        },
+        queryFn() {
+            this.pageNum = 1
+            this.getData()
+        },
+        resetFormFn() {
+            this.sumDate = []
+            this.deptId = ''
+            this.depName = ''
+        },
+        handleSizeChange(val) {
+            console.log(`每页 ${val} 条`);
+        },
+        handleCurrentChange(val) {
+            this.pageNum = val
+            this.getData()
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+  @import "~@/assets/common.less";
+  .view-context {
+    background-color: @color-white;
+    padding: 0 @margin-10;
+    /deep/ .theader-bg {
+      > th {
+        background-color: @bg-th;
+      }
+    }
+    .table-tool {
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: @margin-base 0;
+        > ul {
+            display: flex;
+            margin-left: 20px;
+            > li {
+            color: @color-6c;
+            margin-right: 20px;
+            }
+        }
+    }
+    .color-green {
+        color: @color-green;
+    }
+    .color-red {
+        color: @color-red;
+    }
+  }
+</style>
---
+++ src/components/addPay.vue
@@ -0,0 +1,358 @@
+<template>
+	<div>
+		<el-dialog :title="`${version===1?'新增':version===2?'新增均摊':'编辑'}`+'支出'" width="740px" :visible="getDialogVisible" @close='close'>
+            <el-form :model="ruleForm" ref="ruleForm" label-width="120px">
+                <el-form-item label="部门" v-if="version===1" class="form-item">
+                    <select-tree :data="DepList" :init="ruleForm.deptName" @checkCell="depHandleClick" @clear="clearDep"
+                         @search="searchDep"></select-tree>
+                </el-form-item>
+                <el-form-item label="均摊组织范围" v-if="version===2" class="form-item">
+                    <el-select v-model="ruleForm.depArray" multiple class="w300" filterable :filter-method="depSearch" @visible-change="visibleFn" @remove-tag="delFn">
+                        <el-option value="">
+                            <el-tree :data="DepList" :props="depsProp" accordion @node-click="clickNode"></el-tree>
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="均摊组织类型" v-if="version===2" class="form-item">
+                    <el-radio-group v-model="ruleForm.shareType" @change="checkTypeFn">
+                    <el-radio-button :label="0">分店</el-radio-button>
+                    <el-radio-button :label="1">分组</el-radio-button>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="支出类别" class="form-item">
+                    <el-radio-group v-model="ruleForm.moneyType">
+                    <el-radio-button :label="2">动态成本</el-radio-button>
+                    <el-radio-button :label="3">固定成本</el-radio-button>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="费用项" class="form-item">
+                    <el-input v-model.trim="ruleForm.moneyDepiction" maxlength="100" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="金额" class="money-item form-item">
+                    <el-input v-model="ruleForm.money" @input="cutNumber" clearable></el-input>
+                    <span style="color: red;" v-if="ruleForm.depArray.length&&ruleForm.shareType!==''&&ruleForm.money">
+                        共计{{fiveLevelNum}}个门店（分组），平均每个门店（分组）分摊到{{averageMoney}}元</span>
+                </el-form-item>
+                <el-form-item label="支出时间" class="form-item">
+                    <el-date-picker
+                    v-model="ruleForm.moneyTime"
+                    type="date"
+                    placeholder="选择日期"
+                    :picker-options="pickerOptions">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="备注" class="remark-input">
+                    <el-input type="textarea" v-model.trim="ruleForm.remark" maxlength="250"></el-input>
+                    <span>{{ruleForm.remark.length}}/250</span>
+                </el-form-item>
+                <el-form-item class="dialog-footer">
+                    <div class="next" v-if="version!=3">
+                        <el-checkbox v-model="checked">继续创建下一个</el-checkbox>
+                    </div>
+                    <div class="confirm-btn">
+                        <el-button @click="close">取 消</el-button>
+                        <el-button type="primary" @click="sureFn(version)">确 定</el-button> 
+                    </div>
+                </el-form-item>
+            </el-form>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+import {MIXINS} from "@/assets/js/mixins";
+
+export default {
+  name: 'add-pay',
+  mixins: [MIXINS],
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    },
+    version: {
+        type: Number,
+        default: 1
+    },
+    propData: {
+        type: Object,
+        default() {
+            return {}
+        }
+    }
+  },
+  data() {
+    return {
+        ruleForm: {
+            deptId: '',
+            deptName: '',
+            depArray: [],
+            deps: [],
+            shareType: '',
+            moneyType: '',
+            moneyDepiction: '',
+            money: '',
+            moneyTime: '',
+            remark: ''
+        },
+        pickerOptions: {
+            disabledDate(time) {
+                return time.getTime() > Date.now();
+            }
+        },
+        depsProp: {
+            value: 'depId',
+            children: 'subs',
+            label: 'name'
+        },
+        inputTime: null,
+        checked: false
+    };
+  },
+  created() {
+    this.remoteMethod()
+    // 编辑支出带出赋值
+    if(this.version === 3) {
+        this.ruleForm.deptId = this.propData.deptId
+        this.ruleForm.deptName = this.propData.deptName
+        this.ruleForm.moneyType = this.propData.moneyType
+        this.ruleForm.moneyDepiction = this.propData.moneyDepiction
+        this.ruleForm.money = this.propData.money
+        this.ruleForm.moneyTime = this.propData.moneyTime
+        this.ruleForm.remark = this.propData.remark
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("closePayDialog");
+    },
+    cutNumber() {
+        this.$nextTick(()=>{
+            if(this.ruleForm.money == 0) return this.ruleForm.money = ''
+            this.ruleForm.money = this.$tool.cutFloat({val:this.ruleForm.money,max:999999999.99})
+        })
+    },
+    checkTypeFn(val) {
+        if(!this.ruleForm.depArray.length) {
+            this.ruleForm.shareType = ''
+            this.$message({message: '请先选择均摊组织范围', type: 'error'})
+            return
+        }
+        if(val == 0) {
+            this.ruleForm.deps.find(item => {
+                if(item.level === 5) {
+                    this.ruleForm.shareType = ''
+                    this.$message({message: '当前选择的均摊组织范围不能分店,组织类型请选择分组', type: 'error'})
+                }
+            })
+        } else {
+            let level_arr = []
+            let num_arr = []
+            this.ruleForm.deps.forEach(item => {
+                level_arr.push(item.level)
+                num_arr.push(item.fiveLevelNum)
+            })
+            if(!level_arr.includes(5) && !num_arr.some(item => item>0)) {
+                this.ruleForm.shareType = ''
+                this.$message({message: '当前选择的均摊组织范围不能分组,组织类型请选择分店', type: 'error'})
+            }
+        }
+    },
+    clearDep: function () {
+        this.ruleForm.deptId = ''
+        this.ruleForm.deptName = ''
+    },
+    searchDep: function (payload) {
+        // this.clearSelect('emp')
+    },
+    depHandleClick(data) {
+        this.ruleForm.deptId = data.depId
+        this.ruleForm.deptName = data.name
+    },
+    clickNode(data) {
+        if(this.ruleForm.depArray.includes(data.name)) return
+        this.ruleForm.depArray.push(data.name)
+        this.ruleForm.deps.push(data)
+    },
+    delFn(val) {
+        let _index = ''
+        this.ruleForm.deps.find((item,index) => {
+            if(val === item.name) {
+                _index = index
+            }
+        })
+        this.ruleForm.deps.splice(_index,1)
+    },
+    depSearch(val) {
+        val = val.replace(/\s/g,'')
+        clearTimeout(this.inputTime)
+        this.inputTime = setTimeout(()=>{
+            if(val.length>0){
+                this.getDepList(val)
+            } else {
+                this.initDepList()
+            }
+        },800)
+    },
+    visibleFn(bool) {
+        if(!bool) {
+            this.initDepList()
+        }
+    },
+    initDepList() {
+        this.DepList = this.accountDepList.map(item=>Object.assign({},item))
+    },
+    getDepList(keyword) {
+        this.$ajax.get('/api/access/deps/tree', {
+            keyword
+        }).then(res => {
+            res = res.data
+            if(res.status === 200) {
+                this.DepList = res.data
+            }
+        })
+    },
+    sureFn(v) {
+        let Rules = [
+            { val: this.ruleForm.moneyType, msg: '支出类别' },
+            { val: this.ruleForm.moneyDepiction, msg: '费用项' },
+            { val: this.ruleForm.money, msg: '金额' },
+            { val: this.ruleForm.moneyTime, msg: '支出时间' }
+        ]
+        if(v === 2) {
+            Rules.unshift(
+                { val: this.ruleForm.deps.length, msg: '均摊组织范围' },
+                { val: String(this.ruleForm.shareType), msg: '均摊组织类型' }
+            )
+        } else {
+            Rules.unshift({ val: this.ruleForm.deptId, msg: '部门' })
+        }
+
+        for(let i = 0; i < Rules.length; i++) {
+            if(!Rules[i].val) {
+                this.$message(`${Rules[i].msg}不能为空`)
+                return
+            }
+        }
+        
+        this.ruleForm.moneyTime = this.$tool.dateFormat(this.ruleForm.moneyTime)
+        let param = {...this.ruleForm}
+        let urlStr = 'insertAccountBook'
+        delete param.depArray
+        if(v === 2) {
+            // 新增均摊支出
+            delete param.deptId
+            delete param.deptName
+            let _array = []
+            param.deps.forEach(item => {
+                _array.push(item.depId+','+item.name)
+            })
+            param.deps = _array
+        } else {
+            // 新增支出
+            delete param.deps
+            delete param.shareType
+            if(v === 3) {
+                // 编辑支出
+                urlStr = 'updateAccountBook'
+                param.id = this.propData.id
+            }
+        }
+        this.$ajax.post(`/api/accountBook/${urlStr}`,param).then(res => {
+            res = res.data
+            if(res.status === 200) {
+                if(this.checked) {
+                    this.ruleForm.moneyType = ''
+                    this.ruleForm.moneyDepiction = ''
+                    this.ruleForm.remark = ''
+                } else {
+                    this.close()
+                }
+                this.$message(res.message)
+                // this.$emit('refreshData')
+            }
+        }).catch(error => {
+            this.$message({message: error})
+        })
+    }
+  },
+  computed: {
+    getDialogVisible() {
+      return this.dialogVisible;
+    },
+    fiveLevelNum() {
+        if(this.ruleForm.shareType === 0) {
+            return this.ruleForm.depArray.length
+        } else {
+            let sum = 0
+            this.ruleForm.deps.forEach(item => {
+                if(item.level === 5) {
+                    sum += sum + 1
+                }
+                sum += sum + item.fiveLevelNum
+            })
+            return sum>0 ? sum : 0
+        }
+    },
+    averageMoney() {
+        if(this.ruleForm.shareType === 0) {
+            return (this.ruleForm.money/this.ruleForm.depArray.length).toFixed(2)
+        } else {
+           if(!this.fiveLevelNum) {
+                return 0
+            } else {
+                return (this.ruleForm.money/this.fiveLevelNum).toFixed(2)
+            } 
+        }
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+/deep/ .el-dialog__body {
+    padding: 20px 100px 10px 0;
+}
+.dialog-footer /deep/ .el-form-item__content {
+    display: flex;
+    .next {
+        margin-right: 235px;
+    }
+}
+.money-item /deep/ .el-form-item__content {
+    line-height: 20px;
+}
+.form-item {
+    /deep/ .el-form-item__label::before {
+        content: '*';
+        color: red;
+        margin-right: 2px;
+    }
+}
+/deep/ .tree-box {
+    width: 200px;
+}
+.el-select-dropdown.el-popper {
+    .el-select-dropdown__item {
+      height: 100%;
+      overflow: hidden;
+      padding: 0;
+      .el-tree {
+          margin-right: -17px; /*滚动条的宽度17px*/
+          max-height: 300px;
+          overflow-y: scroll;
+      }
+    }
+}
+.el-radio-button, /deep/ .el-radio-button__inner {
+    width: 100px;
+}
+.remark-input {
+    position: relative;
+    span {
+        position: absolute;
+        bottom: 0;
+        right: 5px;
+    }
+}
+</style>
