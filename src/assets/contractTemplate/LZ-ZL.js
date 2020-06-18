ca2d21c7fac4a70880636295530c2b8e6db162b4
chenwei <chen_wei_rr@163.com>
2020-06-18 10:35:33 +0800

    模板完善

--- src/assets/contractTemplate/LZ-ZL.js
+++ src/assets/contractTemplate/LZ-ZL.js
@@ -12,6 +12,7 @@ let sub = {
   'checkbox_facility': null,
   'val11': null,
   'val12': null,
+  'drapdown_val36': null,
   'val29': null,
   'val30': null,
 }
@@ -61,9 +62,6 @@ contractConfig.inputListener(function(ev,tip){
     ev.target.value=ev.target.value.replace(/[^\d]/g, "")
     tip.target.innerHTML = ev.target.value
     let cn_str = tip.target.getAttribute('extendparam')
-    // if(Obj['cn_arr'].includes(cn_str)){
-    //   document.querySelector(`*[extendparam=${cn_str}_add]`).innerHTML = toChineseNumber(ev.target.value).split("元")[0]
-    // }
   }
 },function(tip){
   //获取输入框的默认值
@@ -79,142 +77,16 @@ contractConfig.inputListener(function(ev,tip){
 })
 
 // 勾选框逻辑
-contractConfig.checkboxListener(function(){},function(obj,index){
-  let attr = obj.currentTarget.getAttribute('name')
-  if(attr == 'houseType') {//第一条 房屋属性 其他类型输入框能否输入逻辑
-    let boxArray = document.getElementsByName(attr);
-    let _val1 = document.querySelector(`span[extendparam="val1"]`)
-    _val1.classList.remove("BODERRED")
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==boxArray.length-1) {
-            _val1.removeAttribute('systemParam')
-          } else {
-            _val1.innerHTML = ''
-            _val1.classList.add("input-before")
-            _val1.setAttribute('systemParam','true')
-          }
-        } else {
-          _val1.innerHTML = ''
-          _val1.classList.add("input-before")
-          _val1.removeAttribute('systemParam')
-        }
-      }
-    }
-  }else if(attr == 'guaranty'){//第二条  抵押人是否可输入逻辑
-    let boxArray = document.getElementsByName(attr);
-    let _val4 = document.querySelector(`span[extendparam="val4"]`)
-    _val4.classList.remove("BODERRED")
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==boxArray.length-1) {
-            _val4.removeAttribute('systemParam')
-          } else {
-            _val4.innerHTML = ''
-            _val4.classList.add("input-before")
-            _val4.setAttribute('systemParam','true')
-          }
-        } else {
-          _val4.innerHTML = ''
-          _val4.classList.add("input-before")
-          _val4.removeAttribute('systemParam')
-        }
-      }
-    }
-  }else if(attr == 'delivery'){//第八条第一小条输入框能否输入逻辑
-    let boxArray = document.getElementsByName(attr);
-    let _val13 = document.querySelector(`span[extendparam="val13"]`)
-    _val13.classList.remove("BODERRED")
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==boxArray.length-1) {
-            _val13.removeAttribute('systemParam')
-          } else {
-            _val13.innerHTML = ''
-            _val13.classList.add("input-before")
-            _val13.setAttribute('systemParam','true')
-          }
-        } else {
-          _val13.innerHTML = ''
-          _val13.classList.add("input-before")
-          _val13.removeAttribute('systemParam')
-        }
-      }
-    }
-  }else if(attr == 'lease'){//第二条 房屋是否带租约勾选逻辑
-    let boxArray = document.getElementsByName(attr);
-    let leaseSon = document.querySelectorAll(`*[name='leaseSon']`)
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==boxArray.length-1) {
-            for (let index = 0; index < leaseSon.length; index++) {
-              leaseSon[index].removeAttribute('readonly')
-            }
-          } else {
-            for (let index = 0; index < leaseSon.length; index++) {
-              leaseSon[index].setAttribute('readonly','readonly')
-              leaseSon[index].querySelector('p').removeAttribute('checked')
-            }
-          }
-        } else {
-          for (let index = 0; index < leaseSon.length; index++) {
-            leaseSon[index].removeAttribute('readonly')
-            leaseSon[index].querySelector('p').removeAttribute('checked')
-          }
-        }
-      }
-    }
-  }else if(attr == 'leakage'){//第六条 房屋是否漏水勾选逻辑
-    let boxArray = document.getElementsByName(attr);
-    let leakagePlace = document.querySelectorAll(`*[name='leakagePlace']`)
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==boxArray.length-1) {
-            for (let index = 0; index < leakagePlace.length; index++) {
-              leakagePlace[index].removeAttribute('readonly')
-            }
-          } else {
-            for (let index = 0; index < leakagePlace.length; index++) {
-              leakagePlace[index].setAttribute('readonly','readonly')
-              leakagePlace[index].querySelector('p').removeAttribute('checked')
-            }
-          }
-        } else {
-          for (let index = 0; index < leakagePlace.length; index++) {
-            leakagePlace[index].removeAttribute('readonly')
-            leakagePlace[index].querySelector('p').removeAttribute('checked')
-          }
-        }
-      }
-    }
-  }else if(attr == 'installment'){//第七条 是否按揭逻辑timeFather
-    let boxArray = document.getElementsByName(attr);
-    let _val10 = document.querySelector(`input[extendparam="val10"]`)
-    _val10.classList.remove("BODERRED")
-    for(let i = 0; i < boxArray.length; i++) {
-      if(boxArray[i] == obj.currentTarget) {
-        if(boxArray[i].querySelector('p').getAttribute('checked')==='true') {
-          if(i==0) {
-            _val10.removeAttribute('disabled')
-          } else {
-            _val10.value=""
-            _val10.removeAttribute('value')
-            _val10.removeAttribute('random')
-            _val10.setAttribute('disabled','true')
-          }
-        } else {
-          _val10.value=""
-          _val10.removeAttribute('value')
-          _val10.removeAttribute('random')
-          _val10.removeAttribute('disabled')
-        }
-      }
-    }
+contractConfig.checkboxListener()
+
+//初始化下拉控件
+Dropdown.create({
+  classN: 'dropdown-item',
+  callBack: function (bindElem, dateObj) {
+    if (bindElem.tagName.toLowerCase() === 'input') {
+      bindElem.value = dateObj.value
+      bindElem.setAttribute('value', bindElem.value)
+    } 
   }
 })
 
@@ -222,26 +94,6 @@ contractConfig.checkboxListener(function(){},function(obj,index){
 let msg = JSON.parse(window.sessionStorage.getItem("contractMsg"));
 for(let readonlyItem in msg){
   let onlyReadDom = Array.from(document.querySelectorAll(`*[systemparam=${readonlyItem}]`));
-  // if(readonlyItem==="guestStoreName"||readonlyItem==="guestStoreRegisterCode"){
-  //   if(msg[readonlyItem].length>0){
-  //     let dom
-  //     if(readonlyItem==="guestStoreName"){
-  //       dom = document.querySelector(`*[extendParam="val5"]`)
-  //     }else{
-  //       dom = document.querySelector(`*[extendParam="val6"]`)
-  //     }
-  //     if(!dom.innerHTML.length>0){
-  //       dom.innerHTML = msg[readonlyItem]
-  //       dom.classList.remove('input-before')
-  //     }
-      
-  //   }
-  // }
-  if(readonlyItem==="timeUnit"){
-    let dom = document.querySelector(`*[systemparam="timeUnit"]`)
-    dom.innerHTML = msg['timeUnit']==="月"?"1":msg['timeUnit']==="季度"?"2":msg['timeUnit']==="半年"?"3":"4"
-    dom.classList.remove('input-before')
-  }
   let readonlyArr = ['code','ownerName','ownerID','guestID','ownerNames','guestName','guestNames','propertyAddr','dealPrice','dealPriceUpper','square']
   if(onlyReadDom.length>0){
     onlyReadDom.forEach((element,index) => {
