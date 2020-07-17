d876e6eccdf35ab9708f032ec3789563f769a198
liufei <837326723@qq.com>
2020-07-17 13:30:20 +0800

    完善1

--- src/pages/setting/contractTemplate.vue
+++ src/pages/setting/contractTemplate.vue
@@ -459,7 +459,7 @@ export default {
                     }
                 })
                 .catch(error => {
-                    console.log(error);
+                    this.$message({message:error})
                 });
         },
         rowOperation: function(row, type, showType) {
