01193cd16bb3e0fc50866bec70c968f0991080b2
liufei <837326723@qq.com>
2020-07-27 20:39:39 +0800

    完善

--- src/pages/setting/company.vue
+++ src/pages/setting/company.vue
@@ -19,7 +19,7 @@
           </el-date-picker>
         </el-form-item>
         <el-form-item label="门店选择">
-          <el-select v-model="searchForm.storeId" multiple filterable collapse-tags value-key="label" @change="depChange"  remote :clearable="true" class="headerDep"  style="width:230px" :remote-method="remoteMethod1" v-loadmore="moreStore1" @visible-change="showView1">
+          <el-select v-model="searchForm.storeId" multiple filterable collapse-tags remote  @change="depChange"   :clearable="true" class="headerDep"  style="width:200px" :remote-method="remoteMethod1" v-loadmore="moreStore1" @visible-change="showView1">
             <el-option v-for="item in homeStoreList" :key="item.id" :label="item.name" :value="item.id">
               <!-- <span style="float: left">{{ item.name.slice(0,8) }}</span> -->
             </el-option>
@@ -1203,36 +1203,13 @@
   }
 }
 .headerDep{
-  display: flex;
-  font-size: 0;
-  // line-height: 30px;
-  // height: 30px;
-  box-sizing: border-box;
-  /deep/ .el-select__tags{
-    // height: 30px;
-    .el-select__input{
-      height: 0;
-    }
-  }
-  /deep/ .el-tag--info{
-    width: 100px;
-    // height: 28px;
+  /deep/.el-select__tags-text{
     display: inline-block;
-            .el-icon-close{
-          top: -5px;
-        }
-    &:nth-of-type(2){
-      margin-left: 40px;
-      width: 40px;
-    }
-    .el-select__tags-text{
-        display: inline-block;
-        width: 100%;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-
-    }
+    max-width: 100px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    vertical-align:middle
   }
 }
 .company-list {
