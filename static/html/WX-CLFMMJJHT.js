e1d3056eaa94de12b68f54f773b26147d1f73c47
疯小孩 <343406717@qq.com>
2020-10-22 16:20:42 +0800

    武穴 模板文件更新

--- static/html/WX-CLFMMJJHT.js
+++ static/html/WX-CLFMMJJHT.js
@@ -1,12 +1,12 @@
 import { contractConfig, toChineseNumber, formatMoney } from "./base.js";
 
 // 动态添加id
-(function() {
-    let t = document.querySelectorAll('.navTitle');
-    t.forEach((e, i) => {
-        e.setAttribute('id', 'a' + i);
-    });
-})()
+// (function() {
+//     let t = document.querySelectorAll('.navTitle');
+//     t.forEach((e, i) => {
+//         e.setAttribute('id', 'a' + i);
+//     });
+// })()
 
 // 获取阿拉伯数字转小写的字符串add
 let Obj;
@@ -25,69 +25,6 @@ let Obj;
     Obj.cn_arr = arr;
 })()
 
-// 必填校验对象
-let sub = {
-    val1: null,
-    val2: null,
-    val3: null,
-    val4: null,
-    val5: null,
-    val6: null,
-    val7: null,
-    val8: null,
-    val9: null,
-    val10: null,
-    val11: null,
-    val12: null,
-    val13: null,
-    val14: null,
-    val15: null,
-    val16: null,
-    val19: null,
-    val20: null,
-    val24: null,
-    val25: null,
-    val26: null,
-    val27: null,
-    val28: null,
-    val29: null,
-    val30: null,
-    val31: null,
-};
-
-// 接口赋值的对象
-//基础数据赋值
-let msg = JSON.parse(window.sessionStorage.getItem("contractMsg"));
-// let msg = {
-//     ownerName: "甲方名字",
-//     ownerID: "甲方身份证号码",
-//     ownerTel: "甲方电话号码",
-//     guestName: "乙方名字",
-//     guestID: "乙方身份证号码",
-//     guestTel: "乙方电话号码",
-//     code: "S0001191107007",
-//     signDate: 1592465819508
-// }
-
-// 赋值
-for (let readonlyItem in msg) {
-    let onlyReadDom = Array.from(document.querySelectorAll(`*[systemparam=${readonlyItem}]`));
-    onlyReadDom.forEach(e => {
-        if (readonlyItem === 'signDate' && msg["signDate"]) {
-            let time = new Date(Number(msg["signDate"]));
-            let y = time.getFullYear();
-            let M = time.getMonth() + 1;
-            let D = time.getDate();
-            let signDate = `${y}年${M}月${D}日`
-            console.log(signDate)
-            e.innerHTML = signDate
-        } else {
-            e.innerHTML = msg[readonlyItem]
-        }
-
-        e.classList.remove('input-before');
-    })
-}
 
 // 下拉初始值
 let dropdownDate = {
@@ -122,10 +59,134 @@ let dropdownDate = {
         init: ['val2011']
     },
 }
+
+// 必填校验对象
+let sub = {
+    val1: null,
+    val2: null,
+    val3: null,
+    val4: null,
+    val5: null,
+    val6: null,
+    val7: null,
+    drapdown_val7: {
+        stateful(val) {
+            return errFn('val7', val);
+        }
+    },
+    val8: null,
+    drapdown_val8: {
+        stateful(val) {
+            return errFn('val8', val);
+        }
+    },
+    val9: null,
+    val10: null,
+    val11: null,
+    val12: null,
+    val13: null,
+    val14: null,
+    val15: null,
+    val16: null,
+    drapdown_val16: {
+        stateful(val) {
+            return errFn('val16', val);
+        }
+    },
+    val19: null,
+    val20: null,
+    drapdown_val20: {
+        stateful(val) {
+            return errFn('val20', val);
+        }
+    },
+    val24: null,
+    val25: null,
+    val26: null,
+    val27: null,
+    val28: null,
+    val29: null,
+    val30: null,
+    val31: null,
+};
+
+function errFn(name, val) {
+    let arr = dropdownDate[name][val];
+    let j = {};
+    arr.forEach(e => {
+        if (e === 'val2011' && name === 'val20') {
+            j['drapdown_val201'] =  {
+                stateful(val) {
+                    return errFn('val201', val);
+                }
+            }
+            return false;
+        }
+        j[e] = null;
+    });
+    return j
+}
+
+// 接口赋值的对象
+//基础数据赋值
+let msg = JSON.parse(window.sessionStorage.getItem("contractMsg"));
+// let msg = {
+//     ownerName: "甲方名字",
+//     ownerID: "甲方身份证号码",
+//     ownerTel: "甲方电话号码",
+//     guestName: "乙方名字",
+//     guestID: "乙方身份证号码",
+//     guestTel: "乙方电话号码",
+//     code: "S0001191107007",
+//     signDate: 1592465819508
+// }
+
+// 赋值
+for (let readonlyItem in msg) {
+    let onlyReadDom = Array.from(document.querySelectorAll(`*[systemparam=${readonlyItem}]`));
+    onlyReadDom.forEach(e => {
+        if (readonlyItem === 'signDate' && msg["signDate"]) {
+            let time = new Date(Number(msg["signDate"]));
+            let y = time.getFullYear();
+            let M = time.getMonth() + 1;
+            let D = time.getDate();
+            let signDate = `${y}年${M}月${D}日`
+            e.innerHTML = signDate
+        } else {
+            e.innerHTML = msg[readonlyItem]
+        }
+
+        e.classList.remove('input-before');
+    })
+}
+
 // 封掉整个下拉关联的值
 for (const key in dropdownDate) {
     if (dropdownDate.hasOwnProperty(key)) {
-        contractConfig.initForm(dropdownDate[key].init, 1)
+
+        // 解封
+        let tag = document.querySelector('[extendParam="' + key + '"]');
+        let v = tag.value || '';
+        let arr1 = dropdownDate[key][v];
+        let arr2 = dropdownDate[key].init;
+        let arr3 = [];
+        let j = {};
+
+        if (v) {
+            // contractConfig.initForm(arr1, 0)
+            // 封
+            arr1.forEach(e => {
+                j[e] = 'a';
+            });
+            arr2.forEach(e => {
+                if (!j[e]) {
+                    arr3.push[e];
+                }
+            });
+            contractConfig.initForm(arr3, 1)
+        }else{
+            contractConfig.initForm(arr2, 1)
+        }
     }
 }
 
@@ -161,6 +222,7 @@ Dropdown.create({
 let mainBtn = document.querySelector('#submit');
 if (mainBtn) {
     mainBtn.addEventListener('click', function(e) {
+        
         contractConfig.submit(e, sub, 'templateError1')
     })
 } else {
@@ -169,7 +231,7 @@ if (mainBtn) {
     btn.style.display = 'none'
     btn.innerHTML = 'click'
     document.body.appendChild(btn)
-    btn.addEventListener('click', function(e) {
+    btn.addEventListener('click', function(e) {    
         contractConfig.submit(e, sub, 'templateError1')
     })
 }
