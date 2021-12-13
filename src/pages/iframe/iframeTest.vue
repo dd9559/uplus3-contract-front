3aa540fb2f40ad912844e360fcb2553314400d0f
lihao <515233780@qq.com>
2021-12-13 11:20:13 +0800

    feat: 2.7.3 委托合同模板

--- src/pages/iframe/iframeTest.vue
+++ src/pages/iframe/iframeTest.vue
@@ -19,7 +19,7 @@ import {MIXINS_DJ} from "../contract/mixins/DJ.js";
     mixins: [MIXINS_MM],
     data(){
       return{
-        src:'wuhan_mr_zl_5.html'
+        src:'wuhan_mr_wt_5.html'
       }
     },
     created(){
