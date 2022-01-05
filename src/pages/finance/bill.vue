6dd9e49a5bfffd18b78d1b22fe207af6cd101b18
heyi <15427458@qq.com>
2022-01-05 10:18:21 +0800

    feat: [2.7.3]-收付款单-退款 改变请求方式

--- src/pages/finance/bill.vue
+++ src/pages/finance/bill.vue
@@ -1681,7 +1681,7 @@ export default {
         // return
         //退款审核
         this.$ajax
-          .post("api/refund/saveOrder", param)
+          .postJSON("/api/refund/saveOrder", param)
           .then(res => {
             let data = res.data;
             if (data.status === 200) {
